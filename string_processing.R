1de5da87a56af8e8bfbe5cd8dfb751578e18b153
Roger D. Peng <rdpeng@gmail.com>
2025-02-24 12:19:29 -0600

    Update

--- string_processing.R
+++ string_processing.R
@@ -4,111 +4,147 @@ library(tidyverse)
 
 competitions <- read_tsv("WCA_export_Competitions.tsv.bz2")
 
+## Look at top of data frame
+competitions
+
+
 ## Look at all data
-competitions |> 
+competitions |>
     glimpse()
 
 ## In what US states are competitions held?
-competitions |> 
-    filter(countryId == "USA") |> 
-    select(starts_with("venue")) 
+competitions |>
+    filter(countryId == "USA") |>
+    select(starts_with("venue"))
 
 ## Restrict columns
-competitions |> 
-    filter(countryId == "USA") |> 
-    select(matches("venue[a-zA-Z]+")) 
+competitions |>
+    filter(countryId == "USA") |>
+    select(matches("venue[a-zA-Z]+"))
 
 ## Take a random sample of competitions
-competitions |> 
-    filter(countryId == "USA") |> 
-    select(venueAddress) |> 
+set.seed(2025-02-24)
+competitions |>
+    filter(countryId == "USA") |>
+    select(matches("venue[a-zA-Z]+")) |>
     sample_n(20)
 
 ## Find competitions in Austin, TX
-competitions |> 
-    filter(countryId == "USA") |> 
-    filter(str_detect(venueAddress, "Austin")) |> 
-    select(name, venueAddress)
-    
+competitions |>
+    filter(countryId == "USA") |>
+    select(name, venueAddress) |>
+    filter(str_detect(venueAddress, "Austin"))
+
 ## Find competitions in Austin, TX
-competitions |> 
-    filter(countryId == "USA") |> 
-    filter(str_detect(venueAddress, 
-                      "Austin, TX|Austin, Texas|Austin TX|Austin Texas")) |> 
-    select(venueAddress, year)
+competitions |>
+    filter(countryId == "USA") |>
+    filter(str_detect(venueAddress,
+                      "Austin, TX|Austin, Texas|Austin TX|Austin Texas")) |>
+    select(name, venueAddress, year)
+
+## Find competitions in Baltimore, MD
+
+
+
+## How many competitions are there in each U.S. state?
 
 ## Select some random rows to get the pattern
-competitions |> 
-    filter(countryId == "USA") |> 
-    select(venueAddress, venueDetails) |> 
+competitions |>
+    filter(countryId == "USA") |>
+    filter(!is.na(venueAddress)) |>
+    select(venueAddress) |>
     sample_n(20)
 
 ## Remove the ZIP code
-competitions |> 
-    filter(countryId == "USA") |> 
-    select(venueAddress) |> 
-    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |> 
-    select(state) |> 
+competitions |>
+    filter(countryId == "USA") |>
+    filter(!is.na(venueAddress)) |>
+    select(venueAddress) |>
+    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |>
+    select(state) |>
     sample_n(20)
 
 
-## Remove trailing USAs
-competitions |> 
-    filter(countryId == "USA") |> 
-    select(venueAddress) |> 
-    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |> 
-    mutate(state = str_replace(state, "USA$|[0-9]+.*USA$", "")) |> 
-    select(state) |> 
+## Remove trailing 'USA's
+competitions |>
+    filter(countryId == "USA") |>
+    filter(!is.na(venueAddress)) |>
+    select(venueAddress) |>
+    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |>
+    mutate(state = str_replace(state, "USA$|[0-9]+.*USA$", "")) |>
+    select(state) |>
     sample_n(20)
 
 ## Remove the street address
-competitions |> 
-    filter(countryId == "USA") |> 
-    select(venueAddress) |> 
-    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |> 
-    mutate(state = str_replace(state, "USA$|[0-9]+.*USA$", "")) |> 
-    mutate(state = str_replace(state, ".*, ", "")) |> 
-    select(state) 
+competitions |>
+    filter(countryId == "USA") |>
+    filter(!is.na(venueAddress)) |>
+    select(venueAddress) |>
+    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |>
+    mutate(state = str_replace(state, "USA$|[0-9]+.*USA$", "")) |>
+    select(state) |>
+    mutate(state = str_replace(state, "^.*, ", ""))
 
 ## Clean up
-competitions |> 
-    filter(countryId == "USA") |> 
-    select(venueAddress) |> 
-    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |> 
-    mutate(state = str_replace(state, "USA$|[0-9]+.*USA$", "")) |> 
-    mutate(state = str_replace(state, ".*, ", "")) |> 
-    mutate(state = str_replace(state, ",$", "")) |> 
-    select(state) |> 
+competitions |>
+    filter(countryId == "USA") |>
+    filter(!is.na(venueAddress)) |>
+    select(venueAddress) |>
+    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |>
+    mutate(state = str_replace(state, "USA$|[0-9]+.*USA$", "")) |>
+    mutate(state = str_replace(state, "^.*, ", "")) |>
+    mutate(state = str_replace(state, ",$", "")) |>
+    select(state) |>
     sample_n(20)
 
 ## Count the number of competitions in each state
-competitions |> 
-    filter(countryId == "USA") |> 
-    select(venueAddress) |> 
-    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |> 
-    mutate(state = str_replace(state, "USA$|[0-9]+.*USA$", "")) |> 
-    mutate(state = str_replace(state, ".*, ", "")) |> 
-    mutate(state = str_replace(state, ",$", "")) |> 
-    filter(state %in% state.name 
-           | state %in% state.abb) |> 
-    select(state) |> 
-    group_by(state) |> 
-    summarize(n = n()) |> 
+competitions |>
+    filter(countryId == "USA") |>
+    filter(!is.na(venueAddress)) |>
+    select(venueAddress) |>
+    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |>
+    mutate(state = str_replace(state, "USA$|[0-9]+.*USA$", "")) |>
+    mutate(state = str_replace(state, "^.*, ", "")) |>
+    mutate(state = str_replace(state, ",$", "")) |>
+    filter(state %in% state.name
+           | state %in% state.abb) |>
+    select(state) |>
+    group_by(state) |>
+    summarize(n = n()) |>
     arrange(state)
 
 ## Bar plot of number of competitions in each state
-competitions |> 
-    filter(countryId == "USA") |> 
-    select(venueAddress) |> 
-    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |> 
-    mutate(state = str_replace(state, "USA$|[0-9]+.*USA$", "")) |> 
-    mutate(state = str_replace(state, ".*, ", "")) |> 
-    mutate(state = str_replace(state, ",$", "")) |> 
-    filter(state %in% state.name 
-           | state %in% state.abb) |> 
-    ggplot(aes(y = state)) + 
+competitions |>
+    filter(countryId == "USA") |>
+    filter(!is.na(venueAddress)) |>
+    select(venueAddress) |>
+    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |>
+    mutate(state = str_replace(state, "USA$|[0-9]+.*USA$", "")) |>
+    mutate(state = str_replace(state, "^.*, ", "")) |>
+    mutate(state = str_replace(state, ",$", "")) |>
+    filter(state %in% state.name
+           | state %in% state.abb) |>
+    ggplot(aes(y = state)) +
     geom_bar()
 
+
+
+## Bar plot of number of competitions in each state w/states ordered
+competitions |>
+    filter(countryId == "USA") |>
+    filter(!is.na(venueAddress)) |>
+    select(venueAddress) |>
+    mutate(state = str_replace(venueAddress, " +[0-9]+$", "")) |>
+    mutate(state = str_replace(state, "USA$|[0-9]+.*USA$", "")) |>
+    mutate(state = str_replace(state, "^.*, ", "")) |>
+    mutate(state = str_replace(state, ",$", "")) |>
+    filter(state %in% state.name
+           | state %in% state.abb) |>
+    group_by(state) |>
+    summarize(n = n()) |>
+    mutate(state = fct_reorder(state, n)) |>
+    ggplot(aes(y = state, x = n)) +
+    geom_col()
 
 
 
